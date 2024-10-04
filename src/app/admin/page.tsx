bf1fb3554705a8920209fdea1c4dbfad28ed012b
Igor Zimmermann <igorzimmermann01@gmail.com>
2024-10-05 00:23:05 +0200

    [ADD] add file upload to `portfolio_backend` service

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,8 +4,42 @@ import Image from 'next/image'
 import { useState } from 'react'
 
 const AdminPage: NextPage = () => {
-	const [imageBufferUrl, setImageBufferUrl] = useState<string | null>(null)
-	const [imageModal, setImageModal] = useState<boolean>(false)
+	const [imageShowModal, setShowImageModal] = useState<boolean>(false)
+
+	const [container] = useState<string>(
+		process.env.NEXT_PUBLIC_PB_DEFAULT_CONTAINER!
+	)
+	const [folder, setFolder] = useState<string>(
+		process.env.NEXT_PUBLIC_PB_DEFAULT_FOLDER!
+	)
+	const [label, setLabel] = useState<string>('')
+	const [file, setFile] = useState<File>()
+
+	const uploadFile = (): Promise<boolean> => {
+		return new Promise((resolve, reject) => {
+			if (!file) return reject('no file')
+			const formData = new FormData()
+			formData.append('container', container)
+			formData.append('folder', folder)
+			formData.append('label', label)
+			formData.append('extension', file.name.split('.').at(-1) || 'jpg')
+			formData.append('files', new Blob([file]))
+
+			fetch(process.env.NEXT_PUBLIC_PB_DEFAULT_UPLOAD_URL!, {
+				method: 'post',
+				body: formData,
+			})
+				.then((resp) => resp.json())
+				.then((data) => {
+					if (data.status === 'okay') {
+						resolve(true)
+					} else {
+						resolve(false)
+					}
+				})
+				.catch((e) => reject(e))
+		})
+	}
 
 	return (
 		<div className="h-[100dvh] w-full flex justify-center items-center bg-black text-white font-sans relative">
@@ -19,7 +53,7 @@ const AdminPage: NextPage = () => {
 							type="text"
 							name="container"
 							id="container"
-							value={'zsoltzimmermann'}
+							value={container}
 							disabled
 							className="bg-white border-0 rounded-none text-base px-2 disabled:hover:cursor-not-allowed"
 						/>
@@ -29,9 +63,13 @@ const AdminPage: NextPage = () => {
 							folder
 						</label>
 						<select
-							name=""
-							id=""
+							name="folder"
+							id="folder"
 							className="bg-white border-0 appearance-none rounded-none text-base px-2"
+							value={folder}
+							onChange={(ev) => {
+								setFolder(ev.target.value)
+							}}
 						>
 							<option value="images">images</option>
 							<hr />
@@ -43,39 +81,70 @@ const AdminPage: NextPage = () => {
 							files
 						</label>
 						<input
-							type="file"
-							name="files"
-							id="files"
-							className="
-                [&::-webkit-file-upload-button]:invisible
-                text-transparent bg-white border-0 appearance-none rounded-none p-2 relative
-                [&::before]:[content:_'upload_file'] [&::before]:block [&::before]:p-2 [&::before]:bg-white [&::before]:text-black [&::before]:text-center [&::before]:absolute [&::before]:top-[50%] [&::before]:left-[50%] [&::before]:[transform:translateX(-50%)_translateY(-50%)] [&::before]:uppercase [&::before]:font-bold"
-							onChange={(ev) => {
-								if (ev.target.files) {
-									setImageBufferUrl(URL.createObjectURL(ev.target.files[0]))
-									ev.target.value = ''
-									setImageModal(true)
-								}
+							type="button"
+							value="upload a file"
+							className="bg-white border-0 rounded-none text-xs p-2 uppercase font-bold hover:cursor-pointer hover:opacity-85"
+							onClick={() => {
+								setShowImageModal(true)
 							}}
 						/>
 					</div>
 				</form>
 			</div>
-			{imageModal && imageBufferUrl && (
-				<div className="z-10 absolute top-[50%] left-[50%] w-[30dvw] bg-black [transform:translateX(-50%)_translateY(-50%)]">
+			{imageShowModal && (
+				<div className="z-10 py-10 absolute top-[50%] left-[50%] w-[30dvw] bg-black [transform:translateX(-50%)_translateY(-50%)]">
 					<form
-						action=""
-						onSubmit={() => {
-							setImageModal(false)
+						onSubmit={async (ev) => {
+							ev.preventDefault()
+
+							uploadFile().then((val) => {
+								if (val === true) {
+									setShowImageModal(false)
+									setLabel('')
+									setFile(undefined)
+								}
+							})
 						}}
 					>
 						<h2 className="uppercase text-xl m-0">upload image</h2>
-						<Image
-							src={imageBufferUrl}
-							alt="xy"
-							height={600}
-							width={600}
-							className="w-auto h-auto max-h-[50dvh] my-5"
+						{file && (
+							<Image
+								src={URL.createObjectURL(file)}
+								alt="uploaded image"
+								width={1200}
+								height={1200}
+								className="h-[50dvh] w-auto w-max-[30dvw]"
+							/>
+						)}
+						<input
+							type="text"
+							name="container"
+							id="container"
+							value={container}
+							className="invisible"
+						/>
+						<input
+							type="text"
+							name="folder"
+							id="folder"
+							value={folder}
+							className="invisible"
+						/>
+						<input
+							type="file"
+							name="files"
+							id="files"
+							className="
+                [&::-webkit-file-upload-button]:invisible
+                text-transparent bg-white border-0 appearance-none rounded-none p-2 relative my-5
+                [&::before]:[content:_'upload_file'] [&::before]:block [&::before]:p-2 [&::before]:bg-white [&::before]:text-black [&::before]:text-center [&::before]:absolute [&::before]:top-[50%] [&::before]:left-[50%] [&::before]:[transform:translateX(-50%)_translateY(-50%)] [&::before]:uppercase [&::before]:font-bold"
+							onChange={async (ev) => {
+								if (ev.target.files) {
+									setFile(ev.target.files[0])
+									setLabel(ev.target.files[0].name.split('.')[0])
+								}
+							}}
+							style={{ display: file ? 'none' : 'initial' }}
 						/>
 						<div className="flex flex-col mb-4">
 							<label htmlFor="label" className="uppercase text-xs font-bold">
@@ -86,6 +155,10 @@ const AdminPage: NextPage = () => {
 								name="label"
 								id="label"
 								className="bg-white border-0 rounded-none text-base px-2"
+								value={label}
+								onChange={(ev) => {
+									setLabel(ev.target.value)
+								}}
 							/>
 						</div>
 						<div className="flex flex-row">
@@ -94,8 +167,8 @@ const AdminPage: NextPage = () => {
 								value="cancel"
 								className="bg-black border-2 border-white border-solid text-white rounded-none text-xs p-2 uppercase font-bold mr-2 hover:cursor-pointer hover:opacity-85"
 								onClick={() => {
-									setImageModal(false)
-									setImageBufferUrl(null)
+									setShowImageModal(false)
+									setFile(undefined)
 								}}
 							/>
 							<input
