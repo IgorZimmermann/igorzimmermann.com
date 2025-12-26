2a0c209130030a4ff8b3b75c6270585e5ff99956
Igor Zimmermann <igorzimmermann01@gmail.com>
2025-12-26 15:44:48 +0100

    :recycle: switch connection to graphql

---
+++ apps/web/app/apollo-client.tsx
@@ -0,0 +1,26 @@
+import { HttpLink } from "@apollo/client"
+import {
+	ApolloClient,
+	InMemoryCache,
+	registerApolloClient,
+} from "@apollo/client-integration-nextjs"
+
+import env from "../lib/env"
+
+export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
+	return new ApolloClient({
+		cache: new InMemoryCache(),
+		link: new HttpLink({
+			// this needs to be an absolute url, as relative urls cannot be used in SSR
+			uri: env.STRAPI_URL,
+			headers: {
+				Authorization: `bearer ${env.STRAPI_TOKEN}`,
+			},
+			fetchOptions: {
+				// you can pass additional options that should be passed to `fetch` here,
+				// e.g. Next.js-related `fetch` options regarding caching and revalidation
+				// see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
+			},
+		}),
+	})
+})
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,23 @@
-import { strapiClient } from "../lib/strapi"
+import { gql } from "@apollo/client"
+
+import { query } from "./apollo-client"
 
 export default async function Home() {
-	const content = await strapiClient.single("homepage").find({ populate: "homepage_grid_items" })
+	const content = await query({ query: gql`
+    query Homepage {
+      homepage {
+        documentId
+        homepage_grid_items {
+          documentId
+          title
+        }
+      }
+    }
+  ` })
 
 	return (
 		<div>
-			{content.data.homepage_grid_items.map((x: { documentId: string, title: string, width: "half" | "full" }) => (
+			{(content.data as { homepage: { documentId: string, homepage_grid_items: [{ title: string, documentId: string }] } }).homepage.homepage_grid_items.map(x => (
 				<p key={x.documentId}>{x.title}</p>
 			))}
 		</div>
