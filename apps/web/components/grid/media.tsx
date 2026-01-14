e8ca36c33056f4c4a9e3dba0af2b764f0e845203
Igor Zimmermann <igorzimmermann01@gmail.com>
2026-01-14 16:10:21 +0100

    :rotating_light: fix media image background overflow

--- apps/web/components/grid/media.tsx
+++ apps/web/components/grid/media.tsx
@@ -17,7 +17,7 @@ type GridItemMediaProps = {
 export default function GridItemMedia({ image, title, link, release }: GridItemMediaProps) {
 	return (
 		<div className={cn("flex justify-center items-center w-full h-full")}>
-			<div className={cn("relative w-full h-full")}>
+			<div className={cn("relative w-full h-full overflow-clip")}>
 				<Image src={env.NODE_ENV === "production" ? image.url : join(env.STRAPI_URL, image.url)} alt={image.alternativeText || ""} width={3000} height={3000} className={cn("h-full w-full object-cover opacity-60 blur-xs")} />
 				<div className={cn("absolute z-10 top-0 left-0 h-full w-full flex flex-col items-center justify-center gap-5")}>
 					<div className={cn("flex flex-col items-center")}>
