69d3c97f328993c643c521aafa4301f9b25d2a79
Igor Zimmermann <igorzimmermann01@gmail.com>
2026-01-22 16:27:54 +0100

    ✨ Feature: bookclub (#2)
    * :sparkles: fetch bookclub details
    
    * :lipstick: display bookclub details
    
    * :lipstick: change not reading message

--- apps/web/components/grid/book-club.tsx
+++ apps/web/components/grid/book-club.tsx
@@ -2,10 +2,12 @@ import { load } from "cheerio"
 import Image from "next/image"
 
 import { cn } from "../../lib/cn"
+import env from "../../lib/env"
+import { loadBookClubSites } from "../../lib/load-bookclub-sites"
 
 export default async function GridItemBookClub() {
 	const resp = await fetch("https://www.goodreads.com/user/show/107252153", { next: {
-		revalidate: 60 * 60 * 12, // 12 hours in seconds,
+		revalidate: 60 * 60, // 1 hour in seconds,
 	} })
 	const $ = load(await resp.text())
 
@@ -13,6 +15,24 @@ export default async function GridItemBookClub() {
 	const bookUrl = `https://www.goodreads.com${$("#currentlyReadingReviews>div.Updates>div.firstcol>a[title]").attr("href")}`
 	const bookAuthor = $("#currentlyReadingReviews>div.Updates>div.secondcol a.authorName").text()
 	const bookImage = $("#currentlyReadingReviews>div.Updates>div.firstcol>a[title]>img[src]").attr("src")?.replace("._SX98_", "._SX800_")
+
+	const bookclub = await loadBookClubSites()
+	let thisIndex: number | null = null
+
+	let previousIndex: number = 0
+	let nextIndex: number = 0
+
+	if (bookclub !== null) {
+		thisIndex = bookclub.sites.findIndex(x => x.startsWith(env.DOMAIN))
+		if (thisIndex === -1) {
+			thisIndex = null
+		}
+
+		if (thisIndex !== null) {
+			previousIndex = (thisIndex - 1 < 0) ? bookclub.sites.length - 1 : thisIndex - 1
+			nextIndex = (thisIndex + 1 >= bookclub.sites.length) ? 0 : thisIndex + 1
+		}
+	}
 
 	return (
 		<div className={cn("flex justify-center items-center w-full h-full")}>
@@ -35,9 +55,37 @@ export default async function GridItemBookClub() {
 						</div>
 					)
 				: (
-						<span>failed loading book</span>
+						<p className={cn("max-w-[40dvw] text-center")}>not reading anything currently</p>
 					)}
-
+			{bookclub !== null && thisIndex !== null && (
+				<>
+					<a
+						href={bookclub.sites[previousIndex]}
+						target="_blank"
+						className={cn("absolute left-2 md:left-10 top-[50%] transform-[translateY(-50%)] hover:underline")}
+					>
+						← previous
+					</a>
+					<a
+						href={bookclub.sites[nextIndex]}
+						target="_blank"
+						className={cn("absolute right-2 md:right-10 top-[50%] transform-[translateY(-50%)] hover:underline")}
+					>
+						next →
+					</a>
+					<p
+						className={cn("absolute left-[50%] bottom-2 transform-[translateX(-50%)] w-full text-center")}
+					>
+						This site is part of the
+						{" "}
+						<a href={bookclub.indexPage} target="_blank" className={cn("hover:underline")}>
+							{bookclub.ringName}
+						</a>
+						{" "}
+						webring.
+					</p>
+				</>
+			)}
 		</div>
 	)
 }
--- apps/web/lib/env.ts
+++ apps/web/lib/env.ts
@@ -4,6 +4,8 @@ import "dotenv/config"
 const envSchema = z.object({
 	NODE_ENV: z.union([z.literal("development"), z.literal("production")]),
 
+	DOMAIN: z.string(),
+
 	STRAPI_MEDIA: z.url(),
 	STRAPI_URL: z.url(),
 	STRAPI_TOKEN: z.string().min(1),
