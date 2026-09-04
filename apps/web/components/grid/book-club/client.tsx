d0f51b180deacddcdd38943465e6b64b0b31bcbd
Igor Zimmermann <igorzimmermann01@gmail.com>
2026-09-04 18:37:10 +0200

    :iphone: fix bookclub grid item responsiveness

--- apps/web/components/grid/book-club/client.tsx
+++ apps/web/components/grid/book-club/client.tsx
@@ -26,8 +26,8 @@ export default function GridItemBookClubClient({ bookTitle, bookUrl, bookAuthor,
 			{(bookImage && bookTitle && bookAuthor && bookUrl)
 				? (
 						<div className={cn("flex flex-col items-center gap-5")}>
-							<Image src={bookImage} alt={bookTitle} width={1000} height={800} className={cn("h-[40dvh] max-h-[40dvw] w-auto")} />
-							<div className={cn("flex flex-col items-center")}>
+							<Image src={bookImage} alt={bookTitle} width={1000} height={800} className={cn("h-[80dvw] sm:h-[35dvw] lg:max-h-[50dvh] w-auto")} />
+							<div className={cn("hidden lg:flex flex-col items-center")}>
 								<h3 className={cn("font-bold text-2xl mt-5 text-ellipsis overflow-hidden max-w-[calc(var(--width)*0.9)] whitespace-nowrap")} title={bookTitle}>{bookTitle}</h3>
 								<span>{bookAuthor}</span>
 							</div>
